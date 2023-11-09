ad72ccd5331202dbeb78279dc2d0fff8f0eee212
Nikhilkumar06 <nikhiltupakula255@gmail.com>
2023-11-09 23:06:07 +0530

    first commit

---
+++ app3/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+def nikhil(request):
+    return render(request,'nikhil.html')
+
+def venky(request):
+    return render(request,'venky.html')
+
+def bhuvi(request):
+    return render(request,'bhuvi.html')
+
+def naveen(request):
+    return render(request,'naveen.html')
+
